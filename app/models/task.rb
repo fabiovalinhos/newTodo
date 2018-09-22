3ba0adc99832fbb113386c66a5b893f97d9a5e83
fabiovalinhos <fabiovalinhos@gmail.com>
2018-09-22 10:38:21 -0300

    Upgrade in UI

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,29 @@
 class Task < ApplicationRecord
   validates :description, presence: true
   validates :done, inclusion: { in: [true, false] }
+
+  def symbol
+    case status
+    when 'pending' then '>>'
+    when 'done' then '√'
+    when 'expired' then 'x'
+    end
+  end
+
+  def css_color
+    case status
+    when 'pending' then 'primary'
+    when 'done' then 'success'
+    when 'expired' then 'danger'
+    end
+  end
+
+  private
+
+  def status
+    return 'done' if done?
+
+    due_date.past? ? 'expired' : 'pending'
+  end
+
 end
